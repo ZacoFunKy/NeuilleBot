fb928b0b4b8745a86f63c75b4cca386406bfb442
ZacoFunKy <zactanji@gmail.com>
2024-08-01 21:31:25 +0200

    5

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Description: This script is used to build and run the docker container for the discord bot.
+
+# Stop the existing discord bot container
+sudo docker stop discord-bot-container
+
+# Remove the existing discord bot container
+sudo docker rm discord-bot-container
+
+# Build the docker image for the discord bot
+sudo docker build -t discord-bot .
+
+# Run the discord bot container with the specified environment file
+sudo docker run -d --name discord-bot-container --env-file .env discord-bot
+
+# Display the logs of the discord bot container
+sudo docker logs discord-bot-container
--- start.sh
+++
@@ -1,6 +0,0 @@
-sudo docker stop discord-bot-container
-sudo docker rm discord-bot-container
-sudo docker build -t discord-bot .
-sudo docker run -d --name discord-bot-container --env-file .env discord-bot
-sudo docker logs discord-bot-container
-
